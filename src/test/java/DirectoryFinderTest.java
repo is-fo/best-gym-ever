9dd65e7a453c7d15c92e351d1acdc3537f81f588
Isak Folke <isvack@gmail.com>
2024-10-15 13:18:56 +0200

    DirectoryFinder unit tests and methods

---
+++ src/test/java/DirectoryFinderTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DirectoryFinderTest {
+
+    DirectoryFinder rootFinder = new DirectoryFinder();
+
+    @Test
+    void getRootDirectory() {
+        String root = System.getProperty("user.dir");
+        assertEquals(root, rootFinder.getRootDirectory());
+    }
+
+    @Test
+    void getMainDirectory() {
+        String main = rootFinder.getRootDirectory() + "\\src\\main\\java";
+        assertEquals(main, rootFinder.getMainDirectory());
+    }
+
+    @Test
+    void getOutputDirectory() {
+        String output = rootFinder.getRootDirectory() + "\\output";
+        assertEquals(output, rootFinder.getOutputDirectory());
+    }
+
+}
