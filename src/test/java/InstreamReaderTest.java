b7600c3706f2cf5118a60ed3f59162ba5764a643
Isak Folke <isvack@gmail.com>
2024-10-15 15:32:57 +0200

    Test for getting a person by their ID from a HashMap

--- src/test/java/InstreamReaderTest.java
+++ src/test/java/InstreamReaderTest.java
@@ -1,7 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InstreamReaderTest {
 
-    InstreamReader instreamReader = new InstreamReader();
+    InstreamReader instreamReader = new InstreamReader("\\testInput.txt");
+    HashMap<Long, Person> personMap = new HashMap<>();
+    Person isak = new Person("0101090058", new NameDate("Isak Folke", LocalDate.now()));
+
+    @Test
+    void readFileToMap() {
+
+    }
+
+    @Test
+    void getPersonByID() {
+        personMap.put(Long.parseLong(isak.getPersonNummer()), isak);
+
+        assertEquals(isak, instreamReader.getPersonByID(
+                Long.parseLong(isak.getPersonNummer()),
+                personMap));
+    }
 
 }
