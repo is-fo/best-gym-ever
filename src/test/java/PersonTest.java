e87e7bb479814ab508ae91dd47905cfbdd855e85
Isak Folke <isvack@gmail.com>
2024-10-22 08:49:39 +0200

    Updated and added tests

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -6,29 +6,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
 
-    Person person = new Person("0101091234", "Isak Folke", LocalDate.parse("2024-10-15"));
 
     @Test
     void calculateMembership() {
+        Person person = new Person("0101091234", "Isak Folke", LocalDate. now());
+
         assertEquals(MemberType.CURRENT, person.calculateMembership());
+        assertEquals(MemberType.FORMER, new Person("0101091234", "Isak Folke", LocalDate. now().minusYears(1)).calculateMembership());
+        assertEquals(MemberType.FORMER, new Person("0101091234",
+                "Isak Folke", LocalDate. now().minusYears(1).minusDays(1)).calculateMembership());
     }
 
     @Test
     void toStringTest() {
+        Person person = new Person("0101091234", "Isak Folke", LocalDate.parse("2024-10-15"));
         assertEquals("0101091234 Isak Folke 2024-10-15", person.toString());
     }
 
     @Test
     void getPersonNummer() {
+        Person person = new Person("0101091234", "Isak Folke", LocalDate. now());
+
         assertEquals("0101091234", person.getPersonNummer());
     }
     @Test
     void getName() {
+        Person person = new Person("0101091234", "Isak Folke", LocalDate. now());
+
         assertEquals("Isak Folke", person.getFullName());
     }
 
     @Test
     void getDate() {
+        Person person = new Person("0101091234", "Isak Folke", LocalDate.parse("2024-10-15"));
+
         assertEquals(LocalDate.parse("2024-10-15"), person.getMembershipDate());
         assertEquals(LocalDate.parse("2024-08-15"),  new Person("0101091234", "Isak Folke", LocalDate.parse("2024-08-15")).getMembershipDate());
     }
